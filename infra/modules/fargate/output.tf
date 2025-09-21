594a951203bf1b5f50f6658ab129cf24847c7da6
Kelechi <kelvinjack005@gmail.com>
2025-09-21 11:07:17 +0100

    reinitialize updated and working repo

--- infra/modules/fargate/output.tf
+++
@@ -1,8 +0,0 @@
-output "fargate_profile_name" {
-  value = aws_eks_fargate_profile.eks_fargate_profile.fargate_profile_name
-}
-
-output "fargate_pods_sg_id" {
-  value = aws_security_group.fargate_pods_sg.id     
-}
-
--- infra/modules/fargate/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "project_name"           { type = string }
-variable "cluster_name"           { type = string }
-variable "pod_execution_role_arn" { type = string }
-variable "subnet_id_1"           { type = string }
-variable "subnet_id_2"           { type = string }
-variable "vpc_id"                 { type = string }
